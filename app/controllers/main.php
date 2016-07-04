79124685c530c8688eecdd9b1d70e7de53116ee0
bencagri <selam@bencagri.com>
2016-07-04 22:37:48 +0300

    main controller var_dump all users

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -5,13 +5,23 @@ use System\Controller;
 class Main extends Controller
 {
 
+	protected $store;
+
 	public function __construct(){
         parent::__construct();
 
+		$this->store = $this->loadHelper('Txtdb',[
+			'dir'      => APP_DIR.'cache/'
+		]);
+
 	}
 
+
 	public function index(){
-		$this->render("index");
+		$data['users'] = $this->store->select('users');
+		$this->render("index",$data);
 	}
+
+
 }
 ?>
--- app/views/index.php
+++ app/views/index.php
@@ -1,1 +1,6 @@
 Hello World!
+
+<?php if($users) : ?>
+    This is our users data.
+    <?php var_dump($users); ?>
+<?php endif; ?>
