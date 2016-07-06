6d722d08df0d0211c8f9e94503156c8b3b7f9a2a
bencagri <selam@bencagri.com>
2016-07-07 00:58:10 +0300

    Sample function has been added to use get_instance

--- app/plugins/trycatch.php
+++ app/plugins/trycatch.php
@@ -11,3 +11,13 @@ function redirect($loc){
     global $config;
     header('Location: '. $config['base_url'] . $loc);
 }
+
+
+/**
+ * Test function for singleton pattern
+ * @param $helper
+ */
+function load_helper($helper){
+    $c = & get_instance();
+    $c->load->helper($helper);
+}
